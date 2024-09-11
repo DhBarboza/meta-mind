e3303511e4c93cb6bad6cfce9d859c621198e8c3
Douglas Henrique <dev.dhbarboza@gmail.com>
2024-09-11 15:19:56 -0300

    Update db settings

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 import { env } from "./source/env";
 
 export default defineConfig({
-    schema: "./source/db/index.ts",
+    schema: "./source/db/schema.ts",
     out: "./.migrations",
     dialect: "postgresql",
     dbCredentials: {

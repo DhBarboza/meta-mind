3ef55078f4afb829b31c70b398f3eca64480af4f
Douglas Henrique <dev.dhbarboza@gmail.com>
2024-09-11 15:06:14 -0300

    create database and add table goals

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 import { env } from "./source/env";
 
 export default defineConfig({
-    schema: "./schema.ts",
+    schema: "./source/db/index.ts",
     out: "./.migrations",
     dialect: "postgresql",
     dbCredentials: {

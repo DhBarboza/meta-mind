16a0da735c759e9bf6a24afe919c87978c6dc109
Douglas Henrique <dev.dhbarboza@gmail.com>
2024-09-12 23:22:07 -0300

    Ajust  sintax

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "drizzle-kit";
-import { env } from "./source/env";
+import { defineConfig } from 'drizzle-kit'
+import { env } from './source/env'
 
 export default defineConfig({
-    schema: "./source/db/schema.ts",
-    out: "./.migrations",
-    dialect: "postgresql",
+    schema: './source/db/schema.ts',
+    out: './.migrations',
+    dialect: 'postgresql',
     dbCredentials: {
         url: env.DATABASE_URL,
     },
-});
+})
